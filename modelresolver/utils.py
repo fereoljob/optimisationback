5b5a2f9b78a315beb8dd7e3b0b313592887fb92e
fereoljob <job.fereol@gmail.com>
2024-02-03 21:29:37 +0100

    partial solution check upgrade

--- modelresolver/utils.py
+++ modelresolver/utils.py
@@ -1,24 +1,67 @@
+import json
 
-
-def checkSolution(solution,partialSolution):
-    tab = partialSolution.split("|")
+def checkCompleteSolution(solution,completeSolution):
+    soluChaine = completeSolution.strip("|")
+    tab = soluChaine.split("|")
     for sol in range(0,solution.__len__()):
-        if compareSol(solution[sol],tab,len(tab)):
+        solverSol = str(solution[sol]).strip("|")
+        solverSolTab = solverSol.split("|")
+        if len(solverSolTab)!=len(tab):
+            return False
+        if compareSolComplete(solution[sol],tab,len(tab)):
             return True
     return False
 
-def compareSol(sol1,soluser,size):
+def checkPartialSolution(solution,partialSolution):
+    tab = partialSolution.split("|")
+    for sol in range(0,solution.__len__()):
+        if compareSolPartial(solution[sol],tab,len(tab)):
+            return True
+    return False
 
-    if(size==0):
+def compareSolComplete(sol1,soluser,size):
+    if size==0:
         return True
-    if checkIfContains(sol1,soluser[size-1]):
-        return compareSol(sol1,soluser,size-1)
+    if checkIfContainsComplete(sol1,soluser[size-1]):
+        return compareSolComplete(sol1,soluser,size-1)
     else:
         return False
     
-def checkIfContains(sol1,ligne):
-    tab = str(sol1).split("|")
-    for i in range(0,len(tab)):
-        if tab[i]==ligne:
+def checkIfContainsComplete(sol1,ligne):
+    solverSol = str(sol1).strip("|")
+    solverSolTab = solverSol.split("|")
+    for i in range(0,len(solverSolTab)):
+        dict1 = json.loads(solverSolTab[i])
+        dict2 = json.loads(ligne)
+        if len(dict1)!=len(dict2):
+            return False
+        else:
+            if dict1==dict2:
+                return True
+    return False
+
+def compareSolPartial(sol1,soluser,size):
+    if size==0:
+        return True
+    if checkIfContainsPartial(sol1,soluser[size-1]):
+        return compareSolPartial(sol1,soluser,size-1)
+    else:
+        return False
+
+def checkIfContainsPartial(sol1,ligne):
+    solverSol = str(sol1).strip("|")
+    solverSolTab = solverSol.split("|")
+    for i in range(0,len(solverSolTab)):
+        dict1 = json.loads(solverSolTab[i])
+        dict2 = json.loads(ligne)
+        if customComparePartial(dict1,dict2,0):
             return True
     return False
+
+def customComparePartial(obj1,obj2,depth):
+    if depth==len(obj2):
+        return True
+    if obj1.get(depth)==obj2.get(depth):
+        return customComparePartial(obj1,obj2,depth+1)
+    else:
+        return False
--- modelresolver/views.py
+++ modelresolver/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from minizinc import Instance, Model, Solver
 import os
-from .utils import checkSolution, compareSol, checkIfContains
+from .utils import checkCompleteSolution,checkPartialSolution
 
 def testPartialSolution(request,problem_id,partialsol):
     model_path = os.path.join('static','modelresolver','model'+str(problem_id)+'.mzn')
@@ -9,7 +9,7 @@ def testPartialSolution(request,problem_id,partialsol):
     solver = Solver.lookup("gecode")
     instance = Instance(solver,model)
     result = instance.solve(all_solutions=True)
-    return HttpResponse(checkSolution(result,partialsol))
+    return HttpResponse(checkPartialSolution(result,partialsol))
 
 def resolveProbleme(request,problem_id,user_solution):
     model_path = os.path.join('static','modelresolver','model'+str(problem_id)+'.mzn')
@@ -17,5 +17,5 @@ def resolveProbleme(request,problem_id,user_solution):
     solver = Solver.lookup("gecode")
     instance = Instance(solver,model)
     result = instance.solve(all_solutions=True)
-    return HttpResponse(checkSolution(result,user_solution))
+    return HttpResponse(checkCompleteSolution(result,user_solution))
 
