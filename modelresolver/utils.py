a3566c73b13414aac0ebea4ae6d9ee8ce29d95ec
fereoljob <job.fereol@gmail.com>
2024-02-03 18:16:31 +0100

    partial solution check

--- modelresolver/utils.py
+++ modelresolver/utils.py
@@ -1,7 +1,31 @@
-def checkProblem(solution,usersolution):
+def checkUserSolution(solution,usersolution):
     tab = usersolution.split("|")
     for sol in range(0,solution.__len__()):
         tab2 = str(solution[sol]).split("|")
         if tab2==tab:
+            return True
+    return False
+
+def checkUserPartial(solution,partialSolution):
+    tab = partialSolution.split("|")
+    for sol in range(0,solution._len__()):
+        if compareSol(solution[sol],tab,len(tab)):
+            return True
+    return False
+
+def compareSol(sol1,soluser,size):
+
+    if(size==0):
+        return True
+    if checkIfContains(sol1,soluser[size-1]):
+        return compareSol(sol1,soluser,size-1)
+    else:
+        return False
+    
+def checkIfContains(sol1,ligne):
+    tab = str(sol1).split("|")
+    print(tab)
+    for i in range(0,len(tab)-1):
+        if tab[i]==ligne:
             return True
     return False
--- modelresolver/views.py
+++ modelresolver/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from minizinc import Instance, Model, Solver
 import os
-from .utils import checkProblem
+from .utils import checkUserSolution, compareSol, checkIfContains, checkUserPartial
 
 def testPartialSolution(request,problem_id,partialsol):
-    return HttpResponse("Not yet implemented!")
+    model_path = os.path.join('static','modelresolver','model'+str(problem_id)+'.mzn')
+    model = Model(model_path)
+    solver = Solver.lookup("gecode")
+    instance = Instance(solver,model)
+    result = instance.solve(all_solutions=True)
+    return HttpResponse(checkUserPartial(result,partialsol))
 
 def resolveProbleme(request,problem_id,user_solution):
     model_path = os.path.join('static','modelresolver','model'+str(problem_id)+'.mzn')
@@ -12,5 +17,5 @@ def resolveProbleme(request,problem_id,user_solution):
     solver = Solver.lookup("gecode")
     instance = Instance(solver,model)
     result = instance.solve(all_solutions=True)
-    return HttpResponse(checkProblem(result,user_solution))
+    return HttpResponse(checkUserSolution(result,user_solution))
 
