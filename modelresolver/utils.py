f34c3c29aa507851daf3ede7e4d2217f8c2ff528
fereoljob <job.fereol@gmail.com>
2024-02-03 19:34:11 +0100

    fix bug

--- modelresolver/utils.py
+++ modelresolver/utils.py
@@ -1,12 +1,6 @@
-def checkUserSolution(solution,usersolution):
-    tab = usersolution.split("|")
-    for sol in range(0,solution.__len__()):
-        tab2 = str(solution[sol]).split("|")
-        if tab2==tab:
-            return True
-    return False
 
-def checkUserPartial(solution,partialSolution):
+
+def checkSolution(solution,partialSolution):
     tab = partialSolution.split("|")
     for sol in range(0,solution.__len__()):
         if compareSol(solution[sol],tab,len(tab)):
@@ -24,8 +18,7 @@ def compareSol(sol1,soluser,size):
     
 def checkIfContains(sol1,ligne):
     tab = str(sol1).split("|")
-    print(tab)
-    for i in range(0,len(tab)-1):
+    for i in range(0,len(tab)):
         if tab[i]==ligne:
             return True
     return False
